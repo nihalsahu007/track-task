ab5d67a02a7ac0b1ae3e768372500e205f165deb
nihal <49619696+nihalsahu007@users.noreply.github.com>
2025-05-29 23:09:47 +0530

    Delete sufala directory

--- sufala/track_task/track/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task, TaskActivityLog
-
-# Register your models here.
-
-
-admin.site.register(Task)
-admin.site.register(TaskActivityLog)
-
--- sufala/track_task/track/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminOrAssignedUser(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_staff or obj.assigned_to == request.user
